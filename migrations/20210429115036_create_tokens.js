e9050c103ba7e900b3f90664b3b60662528b3c58
Gergő Móricz <mo.geryy@gmail.com>
2021-04-29 14:33:21 +0200

    [DB Token oops] Update migration to use onDelete/onUpdate CASCADE

--- migrations/20210429115036_create_tokens.js
+++ migrations/20210429115036_create_tokens.js
@@ -3,7 +3,9 @@ exports.up = function (knex) {
   return knex.schema
     .createTable('tokens', (table) => {
       table.increments('id').primary()
-      table.integer('user').notNullable().references('id').inTable('users')
+      table.integer('user').notNullable()
+        .references('id').inTable('users')
+        .onDelete('CASCADE').onUpdate('CASCADE')
       table.integer('app')
       table.string('token').notNullable()
     })
