f932d645bef1492a99831d7cee9a389c2b66dd28
Gergő Móricz <mo.geryy@gmail.com>
2021-04-30 23:20:19 +0200

    [DB oops] Fix seeds to respect DB autoincrement

--- seeds/000_example_user.js
+++ seeds/000_example_user.js
@@ -7,8 +7,8 @@ exports.seed = function (knex) {
       // Inserts seed entries
       const argonOpts = { type: argon2.argon2id }
       return knex('users').insert([
-        { id: 1, username: 'john', password: await argon2.hash('password', argonOpts), name: 'John Doe', summary: 'This is an example profile.' },
-        { id: 2, username: 'jane', password: await argon2.hash('wordpass', argonOpts), name: 'Jane Doe' }
+        { username: 'john', password: await argon2.hash('password', argonOpts), name: 'John Doe', summary: 'This is an example profile.' },
+        { username: 'jane', password: await argon2.hash('wordpass', argonOpts), name: 'Jane Doe' }
       ])
     })
 }
--- seeds/001_example_token.js
+++ seeds/001_example_token.js
@@ -4,11 +4,12 @@ exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('tokens').del()
     .then(async function () {
+      var users = await knex('users').select('*')
       // Inserts seed entries
       const argonOpts = { type: argon2.argon2id }
       return knex('tokens').insert([
-        { id: 1, uid: 1, token: await argon2.hash('mysecrettoken', argonOpts) },
-        { id: 2, uid: 2, token: await argon2.hash('mysecrettoken', argonOpts) }
+        { uid: users[0].id, token: await argon2.hash('mysecrettoken', argonOpts) },
+        { uid: users[1].id, token: await argon2.hash('mysecrettoken', argonOpts) }
       ])
     })
 }
