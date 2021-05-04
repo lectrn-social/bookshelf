4eab8fb7d386880eda8f9d15794372627f4d3aa3
Gergő Móricz <mo.geryy@gmail.com>
2021-05-04 22:53:52 +0200

    Separate env-based config and bootstrapping from webserver logic
    This change was made to make testing easier. Packages like supertest are capable of taking an Express 'app' instance and run tests upon that.
    We can also easily configure multiple server instances in testing without having to worry about environment variables and etc.

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
 require('dotenv').config()
-const Express = require('express')
-const Knex = require('knex')
-const Objection = require('objection')
-const session = require('express-session')
 
-const routers = require('./routers')
-const helpers = require('./helpers')
-require('./models')
-
-// Initialize Express
-const app = Express()
-app.use(Express.json())
-app.use(Express.urlencoded({ extended: true }))
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: process.env.SESSION_SECRET,
-  maxAge: 30 * 24 * 60 * 60 * 1000 // 1 month
-}))
-app.disable('x-powered-by')
-app.set('trust proxy', 1)
-
-// Initialize Knex/Objection
-const knexConfig = require('./knexfile')
-const knex = Knex(process.env.NODE_ENV === 'production' ? knexConfig.production : knexConfig.development)
-Objection.Model.knex(knex)
-
-// Bind routers
-app.use('/.well-known/webfinger', routers.webfinger)
-app.use('/', (req, res, next) => {
-  // This middleware (more of a "splitter") takes care of ActivityPub vs. HTML routing.
-  if (helpers.routing.isActivityPub(req)) return routers.activityPub(req, res, next)
-  else return routers.frontend(req, res, next)
+const app = require('./server')({
+  sessionSecret: process.env.SESSION_SECRET,
+  knexConfig: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 })
 
 // Start Express
---
+++ server.js
@@ -0,0 +1,38 @@
+module.exports = function server (options) {
+  const Express = require('express')
+  const Knex = require('knex')
+  const Objection = require('objection')
+  const session = require('express-session')
+
+  const routers = require('./routers')
+  const helpers = require('./helpers')
+  require('./models')
+
+  // Initialize Express
+  const app = Express()
+  app.use(Express.json())
+  app.use(Express.urlencoded({ extended: true }))
+  app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: options.sessionSecret,
+    maxAge: 30 * 24 * 60 * 60 * 1000 // 1 month
+  }))
+  app.disable('x-powered-by')
+  app.set('trust proxy', 1)
+
+  // Initialize Knex/Objection
+  const knexConfig = require('./knexfile')
+  const knex = Knex(knexConfig[options.knexConfig])
+  Objection.Model.knex(knex)
+
+  // Bind routers
+  app.use('/.well-known/webfinger', routers.webfinger)
+  app.use('/', (req, res, next) => {
+    // This middleware (more of a "splitter") takes care of ActivityPub vs. HTML routing.
+    if (helpers.routing.isActivityPub(req)) return routers.activityPub(req, res, next)
+    else return routers.frontend(req, res, next)
+  })
+
+  return app
+}
