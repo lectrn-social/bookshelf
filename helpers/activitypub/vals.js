31b323a881840f7821839748a74793b8d70bd2e4
Gergő Móricz <mo.geryy@gmail.com>
2021-05-05 20:47:28 +0200

    [helper AP vals] Add actorTypes

--- helpers/activitypub/vals.js
+++ helpers/activitypub/vals.js
@@ -1,5 +1,6 @@
 module.exports = {
   activityTypes: ['Accept', 'Add', 'Announce', 'Arrive', 'Block', 'Create', 'Delete', 'Dislike', 'Flag', 'Follow', 'Ignore', 'Invite', 'Join', 'Leave', 'Like', 'Listen', 'Move', 'Offer', 'Question', 'Reject', 'Read', 'Remove', 'TentativeReject', 'TentativeAccept', 'Travel', 'Undo', 'Update', 'View'],
+  actorTypes: ['Application', 'Group', 'Organization', 'Person', 'Service'],
   objectTypes: ['Article', 'Audio', 'Document', 'Event', 'Image', 'Note', 'Page', 'Place', 'Profile', 'Relationship', 'Tombstone', 'Video'],
   linkTypes: ['Mention']
 }
