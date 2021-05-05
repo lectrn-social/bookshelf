1e69f06956acde53ca5f84501112e07ee5c1a072
Gergő Móricz <mo.geryy@gmail.com>
2021-05-05 18:58:40 +0200

    [Test Style oops]

--- __tests__/helpers/db.js
+++ __tests__/helpers/db.js
@@ -15,16 +15,16 @@ describe('createToken', () => {
     const john = (await models.User.query()
       .where('username', 'john')
       .limit(1))[0]
-    
+
     const plaintext = await db.createToken(john)
     const b64regex = /^(?:[A-Za-z0-9_-]{4})*(?:[A-Za-z0-9_-]{2}==|[A-Za-z0-9_-]{3}=)?$/
-    
+
     expect(b64regex.test(plaintext)).toBe(true)
     expect(plaintext.length).toBe(256)
 
     const tokens = await models.Token.query()
       .where('uid', john.id)
-    
+
     let foundMatchingToken = false
 
     for (const token of tokens) {
