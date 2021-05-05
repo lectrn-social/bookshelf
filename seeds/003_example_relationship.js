951eba458147b96ff91d4a27cc0d78a2a08fb3f7
Gergő Móricz <mo.geryy@gmail.com>
2021-05-05 19:51:09 +0200

    [DB seed] Add Likes and Reblips

--- seeds/003_example_relationship.js
+++ seeds/003_example_relationship.js
@@ -4,13 +4,19 @@ exports.seed = function (knex) {
   return knex('relationships').del()
     .then(async function () {
       const users = await knex('users').select('*')
+      const blips = await knex('blips').select('*').whereIn('uuid', ['3affab31-5204-45eb-9a0c-3c5a5db3f6e3', '64bdcab0-6f86-4ede-8eb4-96ee453005b6'])
 
       // Inserts seed entries
       return knex('relationships').insert([
-        { type: 'Follow', actor_user_id: users[0].id, object_user_id: users[1].id, approved: true, approve_ts: new Date() },
-        { type: 'Follow', actor_user_id: users[1].id, object_user_id: users[0].id, approved: true, approve_ts: new Date() },
-        { type: 'Follow', actor_user_id: users[1].id, object_url: 'https://lectrn.example.com/@alice', approved: true, approve_ts: new Date() },
-        { type: 'Follow', actor_url: 'https://lectrn.example.com/@bob', object_user_id: users[0].id, approved: true, approve_ts: new Date() }
+        { type: 'Follow', actor_user_id: users[0].id, object_user_id: users[1].id },
+        { type: 'Follow', actor_user_id: users[1].id, object_user_id: users[0].id },
+        { type: 'Follow', actor_user_id: users[1].id, object_url: 'https://lectrn.example.com/@alice' },
+        { type: 'Follow', actor_url: 'https://lectrn.example.com/@bob', object_user_id: users[0].id },
+
+        { type: 'Like', actor_user_id: users[0].id, object_blip_id: blips[0].id },
+        { type: 'Like', actor_user_id: users[1].id, object_blip_id: blips[1].id },
+
+        { type: 'Reblip', actor_user_id: users[0].id, object_blip_id: blips[0] }.id
       ])
     })
 }
