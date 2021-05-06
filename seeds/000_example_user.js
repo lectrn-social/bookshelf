0d11d7b3b20347e50e2de258660e5d5236bd627d
Gergő Móricz <mo.geryy@gmail.com>
2021-05-06 18:59:11 +0200

    [DB User seed] Add 3rd user 'jae'

--- seeds/000_example_user.js
+++ seeds/000_example_user.js
@@ -8,7 +8,8 @@ exports.seed = function (knex) {
       const argonOpts = { type: argon2.argon2id }
       return knex('users').insert([
         { username: 'john', password: await argon2.hash('password', argonOpts), name: 'John Doe', summary: 'This is an example profile.' },
-        { username: 'jane', password: await argon2.hash('wordpass', argonOpts), name: 'Jane Doe' }
+        { username: 'jane', password: await argon2.hash('wordpass', argonOpts), name: 'Jane Doe' },
+        { username: 'jae', password: await argon2.hash('pwaosrsd', argonOpts), name: 'Jae' }
       ])
     })
 }
